6c290ece832f073e01f5dd35a8b508e520543b26
dj0hackers <dj0hackers@gmail.com>
2016-03-08 16:06:12 +0100

    IA

--- Thief/src/singlePlayer/artificialIntelligence/IA.java
+++ Thief/src/singlePlayer/artificialIntelligence/IA.java
@@ -1,5 +1,53 @@
 package singlePlayer.artificialIntelligence;
 
+import com.jme3.math.Vector3f;
+
+import control.GameManager;
+import singlePlayer.model.NodeEnemy;
+
 public class IA {
 
+	private final NodeEnemy enemy;
+
+	public IA(final NodeEnemy enemy) {
+		this.enemy = enemy;
+	}
+
+	public void run() {
+		Vector3f thiefLocation = GameManager.getIstance().getNodeThief().getLocalTranslation();
+		Vector3f enemyLocation = this.enemy.getLocalTranslation();
+
+		// TODO attacca
+
+		Vector3f thiefDirection = thiefLocation.subtract(enemyLocation);
+		thiefDirection.y = -2.0f;
+
+		float distance = thiefLocation.distance(enemyLocation);
+
+		this.enemyRotate(this.enemy, thiefDirection);
+		this.enemyTranslate(enemy, thiefDirection);
+
+		
+		System.out.println(distance);
+
+		if (distance < 5) {
+			this.stop();
+		}
+	}
+
+	public void enemyRotate(NodeEnemy enemy, Vector3f thiefDirection) {
+		enemy.getCharacterControl().setViewDirection(thiefDirection);
+		// enemy.getCharacterControl().setWalkDirection(thiefDirection);
+	}
+
+	public void enemyTranslate(NodeEnemy enemy, Vector3f thiefDirection) {
+		// enemy.getCharacterControl().setViewDirection(thiefDirection);
+		enemy.getCharacterControl().setWalkDirection(thiefDirection);
+		enemy.runAnimation();
+	}
+
+	public void stop() {
+		enemyTranslate(enemy, new Vector3f(0, -2, 0));
+		enemy.stopAnimation();
+	}
 }
--- Thief/src/singlePlayer/model/NodeEnemy.java
+++ Thief/src/singlePlayer/model/NodeEnemy.java
@@ -3,9 +3,34 @@ package singlePlayer.model;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
 
+import singlePlayer.artificialIntelligence.IA;
+
 public class NodeEnemy extends NodeCharacter {
+
+	private final IA ia;
 
 	public NodeEnemy(Spatial model, Vector3f dimensionControll, Vector3f intersection) {
 		super(model, dimensionControll, intersection, 50, 10);
+		this.ia = new IA(this);
+	}
+
+	public void runIntelligence() {
+		this.ia.run();
+	}
+
+	public void stopAnimation() {
+		this.channel.setAnim(this.idle);
+	}
+
+	public void runAnimation() {
+		this.channel.setAnim(this.run);
+	}
+
+	public void attack1Animation() {
+		this.channel.setAnim(this.attack1);
+	}
+
+	public void attack2Animation() {
+		this.channel.setAnim(this.attack2);
 	}
 }
