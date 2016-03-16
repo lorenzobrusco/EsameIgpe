12e1eb3b6bc6f7a355796a3a9f8a2fb6d77ffcdc
dj0hackers <dj0hackers@gmail.com>
2016-03-16 11:04:19 +0100

    Solve conflicts

--- Thief/src/main/Main.java
+++ Thief/src/main/Main.java
@@ -52,6 +52,18 @@ public class Main extends SimpleApplication implements ActionListener, ScreenCon
 		// gameSettings.setRenderer("LWJGL-OpenGL2");
 		// app.setSettings(gameSettings);
 		// app.setShowSettings(false);
+//		AppSettings gameSettings = new AppSettings(false);
+//		gameSettings.setResolution(java.awt.Toolkit.getDefaultToolkit().getScreenSize().width,
+//				java.awt.Toolkit.getDefaultToolkit().getScreenSize().height);
+//		gameSettings.setFullscreen(true);
+//		gameSettings.setVSync(true);
+//		gameSettings.setTitle("Thief");
+//		gameSettings.setUseInput(true);
+//		gameSettings.setFrameRate(500);
+//		gameSettings.setSamples(0);
+//		gameSettings.setRenderer("LWJGL-OpenGL2");
+//		app.setSettings(gameSettings);
+//		app.setShowSettings(false);
 
 		// disable statistics
 		app.setDisplayFps(false);
--- Thief/src/singlePlayer/artificialIntelligence/AI.java
+++ Thief/src/singlePlayer/artificialIntelligence/AI.java
@@ -3,6 +3,8 @@ package singlePlayer.artificialIntelligence;
 import com.jme3.bounding.BoundingBox;
 import com.jme3.collision.CollisionResult;
 import com.jme3.collision.CollisionResults;
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 
 import control.GameManager;
@@ -11,71 +13,126 @@ import singlePlayer.model.NodeModel;
 
 public class AI {
 
-	private final NodeEnemy enemy;
-	private final float SPEED = 15;
+    private final NodeEnemy enemy;
+    private final Way way;
+    private final float SPEED = 15;
+    private boolean stopSearchThief;
+    private Vector3f newWay;
 
-	public AI(final NodeEnemy enemy) {
-		this.enemy = enemy;
-	}
+    public AI(final NodeEnemy enemy) {
+	this.enemy = enemy;
+	this.stopSearchThief = false;
+	this.newWay = new Vector3f(0f, 0f, 0f);
+	this.way = new Way(this.enemy);
 
-	public void run() {
+    }
 
-		Vector3f thiefLocation = GameManager.getIstance().getNodeThief().getLocalTranslation();
-		Vector3f enemyLocation = this.enemy.getLocalTranslation();
+    public void run() {
 
-		Vector3f thiefDirection = thiefLocation.subtract(enemyLocation);
-		thiefDirection.y = -2.0f;
-
-		float distance = thiefLocation.distance(enemyLocation);
-
-		if (distance > this.enemy.getDISTANCE()) {
-			this.enemy.setHasFound(false);
-			this.enemyRotate(this.enemy, thiefDirection);
-			this.enemyTranslate(enemy, thiefDirection, distance);
-			if (this.collisionResults()) {
-				new Way(this.enemy);
-			}
-		} else if (distance <= this.enemy.getDISTANCE() && !this.enemy.hasFound()) {
-			stop();
-			this.enemy.setHasFound(true);
+	if (!stopSearchThief) {
+	    Vector3f thiefLocation = GameManager.getIstance().getNodeThief().getLocalTranslation();
+	    Vector3f enemyLocation = this.enemy.getLocalTranslation();
+	  
+	    Vector3f thiefDirection = thiefLocation.subtract(enemyLocation);
+	    thiefDirection.y = -2.0f;
+	   
+	    float distance = thiefLocation.distance(enemyLocation);
+	    this.enemyRotate(this.enemy, thiefDirection);
+	    if (distance > this.enemy.getDISTANCE()) {
+		this.enemy.setHasFound(false);
+		this.enemyTranslate(enemy, thiefDirection, distance);
+		if (this.collisionResults((BoundingBox) this.enemy.getBox().getWorldBound())) {
+		    this.stopSearchThief = true;
+		    this.way.newWay();
+		    if (!this.collisionResults((BoundingBox) this.way.getRightBox().getWorldBound())){
+			this.newWay = thiefDirection.add(new Vector3f(5f,0,0));
+			
+		    }
+		    else if (!this.collisionResults((BoundingBox) this.way.getLeftBox().getWorldBound())){
+			this.newWay = thiefDirection.add(new Vector3f(-5f,0,0));
+			
+		    }
+//		    else {
+//			this.newWay = this.way.getBackBox().getLocalTranslation();
+//			this.newWay = this.way.getBackBox().getLocalTranslation();
+//			System.out.println(this.newWay);
+//		    }
+		    this.alternativeWay(this.newWay);
 		}
-	}
+	    } else if (distance <= this.enemy.getDISTANCE() && !this.enemy.hasFound()) {
+		stop();
+		this.enemy.setHasFound(true);
+	    }
+	} else
+	    controll(this.enemy.getLocalTranslation(), this.newWay);
+    }
 
-	public boolean collisionResults() {
-		boolean collision = false;
-		for (NodeModel node : GameManager.getIstance().getModels()) {
-			if (node == this.enemy || node == GameManager.getIstance().getNodeThief())
-				continue;
-			CollisionResults collisionResult = new CollisionResults();
-			BoundingBox box = (BoundingBox) this.enemy.getBox().getWorldBound();
+    public void alternativeWay(Vector3f direction) {
 
-			box.setXExtent(2.5f);
-			box.setYExtent(1.0f);
-			box.setZExtent(2.0f);
+	Vector3f enemyLocation = this.enemy.getLocalTranslation();
+	Vector3f view=this.enemy.getCharacterControl().getViewDirection();
+	Quaternion rotateL = new Quaternion().fromAngleAxis(FastMath.PI /8, Vector3f.UNIT_Y);
+	rotateL.multLocal(view);
+	enemyRotate(enemy, view);
+	
+	
+//	this.newWay = enemyLocation.subtract(direction);
+//	this.newWay.y = -2f;
+//	System.out.println(newWay);
+//	float distance = enemyLocation.distance(direction);
+//	this.enemyRotate(this.enemy, direction);
+//	this.enemyTranslate(this.enemy, direction, distance);
 
-			node.getModel().collideWith(box, collisionResult);
+    }
 
-			CollisionResult closest = collisionResult.getClosestCollision();
-
-			if (closest != null) {
-				collision = true;
-			}
+    public void controll(Vector3f start, Vector3f end) {
+	// System.out.println(start + "----" + end);
+	new Thread(){
+	    @Override
+	    public void run() {
+		try {
+		    sleep(1000);
+		    AI.this.stopSearchThief = false;
+		} catch (InterruptedException e) {
+		    // TODO Auto-generated catch block
+		    e.printStackTrace();
 		}
-		return collision;
-	}
+	    }
+	}.start();
+    }
+    
 
-	public void enemyRotate(NodeEnemy enemy, Vector3f thiefDirection) {
-		enemy.getCharacterControl().setViewDirection(thiefDirection);
-	}
+    public boolean collisionResults(BoundingBox box) {
+	boolean collision = false;
+	for (NodeModel node : GameManager.getIstance().getModels()) {
+	    if (node == this.enemy || node == GameManager.getIstance().getNodeThief())
+		continue;
+	    CollisionResults collisionResult = new CollisionResults();
+	    box.setXExtent(2.5f);
+	    box.setYExtent(1.0f);
+	    box.setZExtent(2.0f);
+	    node.getModel().collideWith(box, collisionResult);
+	    CollisionResult closest = collisionResult.getClosestCollision();
 
-	public void enemyTranslate(NodeEnemy enemy, Vector3f thiefDirection, float distance) {
-		enemy.getCharacterControl().setWalkDirection(thiefDirection.mult((1 / distance) * this.SPEED));
-		enemy.runAnimation();
+	    if (closest != null) {
+		collision = true;
+	    }
 	}
+	return collision;
+    }
 
-	public void stop() {
-		enemyTranslate(enemy, new Vector3f(0, -2, 0), 00);
-		enemy.stopAnimation();
-	}
+    public void enemyRotate(NodeEnemy enemy, Vector3f thiefDirection) {
+	enemy.getCharacterControl().setViewDirection(thiefDirection);
+    }
+
+    public void enemyTranslate(NodeEnemy enemy, Vector3f thiefDirection, float distance) {
+	enemy.getCharacterControl().setWalkDirection(thiefDirection.mult((1 / distance) * this.SPEED));
+	enemy.runAnimation();
+    }
+
+    public void stop() {
+	enemyTranslate(enemy, new Vector3f(0, -2, 0), 00);
+	enemy.stopAnimation();
+    }
 
 }
